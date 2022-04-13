fd8bdbc6b2ee3e26524adfe9eabfab2745ebdf19
TomDeylen <tdeylen@gmail.com>
2022-04-13 15:12:42 +1000

    Added C# Gamertag example and Movement example

---
+++ C#/ICTPRG302 Intro to Programming/ICTPRG302 Intro to Programming/Gamertags.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ICTPRG302_Intro_to_Programming
+{
+    class Gamertags
+    {
+        private string[] gamerTagList = { };
+        int amountToShow = 5;
+        bool uppercase = true;
+
+        public void LoadGamertags()
+        {
+            gamerTagList = File.ReadAllLines("../Gamertags.txt");
+        }
+
+        public void PrintAllGamerTags()
+        {
+            Console.Clear();
+            Console.WriteLine("=======================");
+            Console.WriteLine("All GamerTags");
+            Console.WriteLine("=======================");
+
+            int lineNumber = 1;
+            foreach (string s in gamerTagList)
+            {
+                string currentTag = s;
+                if (uppercase == true)
+                    currentTag = currentTag.ToUpper();
+                Console.WriteLine(lineNumber.ToString() + ") " + currentTag);
+                lineNumber = lineNumber + 1;
+                if (amountToShow < lineNumber)
+                    break;
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+        public void PrintGamertagsEndingWithNumber()
+        {
+            Console.Clear();
+            Console.WriteLine("=======================");
+            Console.WriteLine("All GamerTags");
+            Console.WriteLine("=======================");
+
+            int lineNumber = 1;
+            foreach (string s in gamerTagList)
+            {
+                if ((s.Length > 0) && Char.IsNumber(s, s.Length - 1))
+                {
+                    string currentTag = s;
+                    if (uppercase == true)
+                        currentTag = currentTag.ToUpper();
+                    Console.WriteLine(lineNumber.ToString() + ") " + currentTag);
+                    lineNumber = lineNumber + 1;
+                    if (lineNumber == amountToShow)
+                        break;
+                }
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        public void PrintGamertagsNotStartingWithNumberOrLetter()
+        {
+            Console.Clear();
+            Console.WriteLine("=======================");
+            Console.WriteLine("All GamerTags");
+            Console.WriteLine("=======================");
+
+            int lineNumber = 1;
+            foreach (string s in gamerTagList)
+            {
+                if ((s.Length > 0) && Char.IsLetterOrDigit(s, 0) == false)
+                {
+                    string currentTag = s;
+                    if (uppercase == true)
+                        currentTag = currentTag.ToUpper();
+                    Console.WriteLine(lineNumber.ToString() + ") " + currentTag);
+                    lineNumber = lineNumber + 1;
+                    if (lineNumber == amountToShow)
+                        break;
+                }
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/ICTPRG302 Intro to Programming/ICTPRG302 Intro to Programming/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace ICTPRG302_Intro_to_Programming
+{
+	class Program
+	{
+		/// <summary>
+		/// This is the starting location for the program.
+		/// </summary>
+		/// <param name="args">
+		/// A list of strings passed in to the program
+		/// from the command-line when it was started
+		/// </param>
+		static void Main(string[] args)
+		{
+            Gamertags gamertags = new Gamertags();
+            gamertags.LoadGamertags();
+            //gamertags.PrintAllGamerTags();
+            gamertags.PrintAllGamerTags();
+        }
+	}
+}
